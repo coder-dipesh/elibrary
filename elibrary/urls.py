ed6b73685626127c875b0769f54d9a06e8ab2017
coder-dipesh <dipeshsiwakoti112@gmail.com>
2022-02-02 15:31:32 +0545

    Initial Project setup!! Ready to go :fire:

---
+++ elibrary/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('accounts.urls')),
+    path('admins/', include('admins.urls')),
+    path('books/', include('books.urls')),
+]
